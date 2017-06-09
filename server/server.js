15da6da1b87f3715bbd10a6d19d6f1c67fd2d3f1
Karthik Pandurangan <kpandurangan@FORZACONSULTING.local>
2017-06-09 16:12:56 +0200

    Client server example

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-    console.log (`Server is running on port ${port}`)
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
