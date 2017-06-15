92935e1bfcac75ba5b82d9d3405e1a41f9d8463f
Karthik Pandurangan <kpandurangan@FORZACONSULTING.local>
2017-06-15 10:47:40 +0200

    Add join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -51,6 +51,7 @@ socket.on('broadCastMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 // socket.on('welcomeMessage', function (message) {
@@ -93,6 +94,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
@@ -129,3 +131,20 @@ locationButton.on('click', function(){
         alert('Unable to fetch location.');
     })
 })
+
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
